b27f970e5b4689a4f2b3b37f0adadc6323814d42
annapav7 <pavlenkoanna2011@gmail.com>
2024-03-15 23:14:16 -0600

    Guided_LAB_302.2.2_Core_Jave_Type_Casting

---
+++ src/AutoTypeConversion.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// Author: Pavlenko Anna
+// Data: 3/15/2024
+public class AutoTypeConversion {
+    // Example: Implicit Type Casting
+    public static void main(String[] args) {
+
+            int x = 20;
+            double y = 40.5;
+            long p = 30;
+            float q = 10.60f;
+            // int z = x + y; (1) // Error; cannot convert from double to int.
+            double z = x + y;
+            System.out.println("Sum of two numbers: " + z);
+
+            // long r = p - q; // (2) // Error; cannot convert from float to long.
+            float r = p - q;
+            System.out.println("Subtraction of two numbers: " + r);
+    }
+}
